vg2signal.py: -implement UnivariateSpline(k=3,s=0) to prevent smoothing groupvg2.py -added average signal to param_analysis print out -fixed run_folderpath to run all iterations of params -added peak voltage to signal.xlsx files

vg2signal.py
@@ -90,23 +90,24 @@ def make_signal_getter(vstart: float,
     def signal_getter_func(v: numpy.array,
                            lisd: numpy.array):
         v_in = numpy.logical_and(v >= vstart, v <= vend)
-        sg = scipy.signal.savgol_filter(lisd[v_in],window_length=5,polyorder=3, mode="nearest")
-        spline_model = scipy.interpolate.CubicSpline(v[v_in], sg)
-        spline_model_d = spline_model.derivative(nu=1)
-        spline_model_dd = spline_model.derivative(nu=2)
-        roots_d = spline_model_d.roots()
+        spline_model = scipy.interpolate.UnivariateSpline(v[v_in],
+                                                          lisd[v_in],
+                                                          k=3)
+        spline_model_d = spline_model.derivative(n=1)
+        spline_model_d_ppoly = scipy.interpolate.splrep(v[v_in],
+                                                        list(map(spline_model_d,
+                                                                 v[v_in])), k=3)
+        roots_d = scipy.interpolate.PPoly.from_spline(spline_model_d_ppoly).roots(extrapolate=False)
+        spline_model_dd = numdifftools.Derivative(spline_model, n=2)
         dd_at_roots = numpy.array(list(map(spline_model_dd, roots_d)))
         critical_point_v = None
-        print(dd_at_roots)
         if len(dd_at_roots) > 0:
             ind_peak = numpy.argmin(dd_at_roots)
             if dd_at_roots[ind_peak] < 0:
-                print("in")
                 critical_point_v = roots_d[ind_peak]
         signal = None
         if critical_point_v is not None:
             signal = -dd_at_roots[ind_peak]
-        #print(signal,critical_point_v)
         return (signal, critical_point_v)
     return signal_getter_func
 